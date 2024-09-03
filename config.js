c614779b0595a2d97aa5beebed46da7ed08c35dc
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-03 09:47:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'inrl~05a1fMSt1f59d02b25d491454349196c59a1::13', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~89fd5hYLr2dfed145ab5aff4557078bc662c::9', //your ssid to run bot
     PORT: process.env.PORT || 3000,
     BASE_URL : "https://web-pair-upf6.onrender.com/",
-    PREFIX: '.',
+    PREFIX: '','.',
     WORKTYPE: 'private',
-    SUDO: '',
+    SUDO: '94782543893',
     REPO: "inr-l/inrl-bot-md",
     DATABASE: DB_URL ? new Sequelize(DB_URL,{dialect:'postgres',ssl:true,protocol: 'postgres', dialectOptions: {native: true,ssl:{require: true,rejectUnauthorized: false}}, logging: false}) : new Sequelize({dialect:'sqlite',storage:'./database.db',logging:false}) 
 };
